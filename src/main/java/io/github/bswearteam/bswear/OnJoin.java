e81cc848d087d458378566bf6c606025f9bc76eb
Isaiah Patton <isaiahpatton1@gmail.com>
2016-09-04 10:09:46 -0400

    Update OnJoin.java

--- src/main/java/io/github/bswearteam/bswear/OnJoin.java
+++ src/main/java/io/github/bswearteam/bswear/OnJoin.java
@@ -1,27 +1,28 @@
 package io.github.bswearteam.bswear;
  
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.configuration.file.FileConfiguration;
-
-import io.github.bswearteam.bswear.Main;
 
 public class OnJoin implements Listener {
 
-    //final FileConfiguration config = Bukkit.getPluginManager().getPlugin("BSwear").getConfig();
+    private Main main;
 
+	public OnJoin(Main m) {
+		main = m;
+	}
+	
+	
     String pwar = "2";
     String maxwar = "2";    
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         String JoinPrefix = ChatColor.BLUE + "[" + ChatColor.AQUA + "BSwear" + ChatColor.BLUE + "]" + " ";
+        JoinPrefix = main.prefix;
         player.sendMessage(JoinPrefix + ChatColor.GRAY + "Our Antiswearing filter is protecting this server!");
-        player.sendMessage(JoinPrefix + ChatColor.GRAY + "You have " + pwar + "/" + maxwar + " Warnings left");
+        //TODO player.sendMessage(JoinPrefix + ChatColor.GRAY + "You have " + pwar + "/" + maxwar + " Warnings left");
     }
 }
