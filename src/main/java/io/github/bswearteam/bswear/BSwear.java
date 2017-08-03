d44f452dfe1fb14c0cb57dfc0b8fd4da0245f736
maede97 <maede97@hotmail.com>
2017-08-03 17:48:40 +0200

    Updater Function
    Newer Versions of the plugin will now self-check if there's an update.
    Verions-Numbers must be manually added in the file "version" in the root
    of this rep.
    The function then logs a message if it's up-to-date or if not.

--- src/main/java/io/github/bswearteam/bswear/BSwear.java
+++ src/main/java/io/github/bswearteam/bswear/BSwear.java
@@ -1,10 +1,14 @@
 package io.github.bswearteam.bswear;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.InvalidConfigurationException;
@@ -20,188 +24,252 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BSwear extends JavaPlugin implements Listener {
-    public PluginDescriptionFile pdf = this.getDescription();
-    public String version = pdf.getVersion();
+	public PluginDescriptionFile pdf = this.getDescription();
+	public String version = pdf.getVersion();
 
-    public static Permission BypassPerm = new Permission("bswear.bypass");
-    public Permission CommandPerm       = new Permission("bswear.command.use");
-    public Permission allPerm           = new Permission("bswear.*");
-    public Permission AdvertisingBypass = new Permission("bswear.advertising.bypass");
-    public FileConfiguration config = new YamlConfiguration();
-    public FileConfiguration swears = new YamlConfiguration();
-    public FileConfiguration swearers = new YamlConfiguration();
-    public FileConfiguration muted = new YamlConfiguration();
-    public FileConfiguration log = new YamlConfiguration();
-    public String prefix = ChatColor.GOLD + "[BSwear] "+ChatColor.GREEN;
-    public File configf,swearf,swearersf,logFile,mutedf;
-    public ArrayList<String> logtext = new ArrayList<>();
+	public static Permission BypassPerm = new Permission("bswear.bypass");
+	public Permission CommandPerm = new Permission("bswear.command.use");
+	public Permission allPerm = new Permission("bswear.*");
+	public Permission AdvertisingBypass = new Permission("bswear.advertising.bypass");
+	public FileConfiguration config = new YamlConfiguration();
+	public FileConfiguration swears = new YamlConfiguration();
+	public FileConfiguration swearers = new YamlConfiguration();
+	public FileConfiguration muted = new YamlConfiguration();
+	public FileConfiguration log = new YamlConfiguration();
+	public String prefix = ChatColor.GOLD + "[BSwear] " + ChatColor.GREEN;
+	public File configf, swearf, swearersf, logFile, mutedf;
+	public ArrayList<String> logtext = new ArrayList<>();
 
-    @Override
-    public void onEnable() {
-        PluginManager pm = Bukkit.getServer().getPluginManager();
-        //pm.addPermission(BypassPerm);
-        //pm.addPermission(CommandPerm);
-        //pm.addPermission(AdvertisingBypass);
+	@Override
+	public void onEnable() {
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		// pm.addPermission(BypassPerm);
+		// pm.addPermission(CommandPerm);
+		// pm.addPermission(AdvertisingBypass);
 
-        configf = new File(getDataFolder(), "config.yml");
-        swearf = new File(getDataFolder(), "words.yml");
-        swearersf = new File(getDataFolder(), "swearers.yml");
-        mutedf = new File(getDataFolder(), "mutedPlayers.yml");
-        logFile = new File(getDataFolder(), "log.yml");
+		configf = new File(getDataFolder(), "config.yml");
+		swearf = new File(getDataFolder(), "words.yml");
+		swearersf = new File(getDataFolder(), "swearers.yml");
+		mutedf = new File(getDataFolder(), "mutedPlayers.yml");
+		logFile = new File(getDataFolder(), "log.yml");
 
-        resourceSave(configf, "config.yml");
-        resourceSave(swearf, "words.yml");
-        resourceSave(swearersf, "swearers.yml");
-        resourceSave(mutedf, "mutedPlayers.yml");
+		resourceSave(configf, "config.yml");
+		resourceSave(swearf, "words.yml");
+		resourceSave(swearersf, "swearers.yml");
+		resourceSave(mutedf, "mutedPlayers.yml");
 
-        if (!logFile.exists()){
-            try {
-                logFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+		if (!logFile.exists()) {
+			try {
+				logFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-        try {
-            config.load(configf);
-            swears.load(swearf);
-            swearers.load(swearersf);
-            muted.load(mutedf);
-            log.load(logFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
+		try {
+			config.load(configf);
+			swears.load(swearf);
+			swearers.load(swearersf);
+			muted.load(mutedf);
+			log.load(logFile);
+		} catch (IOException | InvalidConfigurationException e) {
+			e.printStackTrace();
+		}
 
-        saveDefaultConfig();
+		saveDefaultConfig();
 
-        // Shows an message saying BSwear is enabled
-        if (getConfig().getBoolean("showEnabledMessage")) {
-            getLogger().info("[=-=] BSwear team [=-=]");
-            getLogger().info("This server runs BSwear v"+version);
-            getLogger().info("- ClusterAPI by AdityaTD");
-        }
+		// Shows an message saying BSwear is enabled
+		if (getConfig().getBoolean("showEnabledMessage")) {
+			getLogger().info("[=-=] BSwear team [=-=]");
+			getLogger().info("This server runs BSwear v" + version);
+			getLogger().info("- ClusterAPI by AdityaTD");
+		}
 
-        // Checks if both ban and kick are set to true
-        if (getConfig().getBoolean("banSwearer") && getConfig().getBoolean("kickSwearer")) getConfig().set("banSwearer", false);
+		// Checks if both ban and kick are set to true
+		if (getConfig().getBoolean("banSwearer") && getConfig().getBoolean("kickSwearer"))
+			getConfig().set("banSwearer", false);
 
-        // sets the prefix
-        if (getConfig().getString("messages.prefix") == null) getConfig().set("messages.prefix", "&6[BSwear]&2");
-        prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.prefix")) + " ";
+		// sets the prefix
+		if (getConfig().getString("messages.prefix") == null)
+			getConfig().set("messages.prefix", "&6[BSwear]&2");
+		prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.prefix")) + " ";
 
-        pm.registerEvents(this, this);
-        getCommand("mute").setExecutor(new Mute(this));
-        getCommand("bswear").setExecutor(new BSwearCommand(this));
-        getCommand("swear").setExecutor(new SwearCommand(this));
-        registerEvents(pm, this, this, new OnJoin(this), new Mute(this), new Advertising(this));
-    }
-    
-    public static void registerEvents(PluginManager p, org.bukkit.plugin.Plugin plugin, Listener... listeners) {
-        for (Listener lis : listeners) p.registerEvents(lis, plugin);
-    }
+		pm.registerEvents(this, this);
+		getCommand("mute").setExecutor(new Mute(this));
+		getCommand("bswear").setExecutor(new BSwearCommand(this));
+		getCommand("swear").setExecutor(new SwearCommand(this));
+		registerEvents(pm, this, this, new OnJoin(this), new Mute(this), new Advertising(this));
+		checkForUpdate();
+	}
 
-    /*Controls config files*/
-    public void saveSwearConfig() { saveConf(swears, swearf); }
-    public void saveSwearersConfig() { saveConf(swearers, swearersf); }
+	public static void registerEvents(PluginManager p, org.bukkit.plugin.Plugin plugin, Listener... listeners) {
+		for (Listener lis : listeners)
+			p.registerEvents(lis, plugin);
+	}
 
-    private String lastblockmsg = "_null_";
-    @EventHandler/*(priority = EventPriority.HIGHEST)*/
-    public void onChatSwear(AsyncPlayerChatEvent event) {
-        if (true/*!event.getPlayer().hasPermission(BypassPerm)*/) {
-            String message = replaceAllNotNormal(event.getMessage().toLowerCase().replaceAll("[%&*()$#!-_@]", ""));
-            boolean has = false;
-            boolean allowUsersToSeeWords = true; // TODO: add to configuration
-            String messagewithoutswear = message;
-            int i = 0;
-            /*swears.getStringList("warnList").stream().forEach((word) -> {*/
-            for (String word : swears.getStringList("warnList")) {
-                if (ifHasWord(message, word)) {
-                    has = true;
-                    event.setCancelled(true); // BSwear handles sending the message so cancel the event.
-                    if (getConfig().getBoolean("cancelMessage") == true) {
-                        has = false; // cancel message.
-                    } else {
-                        messagewithoutswear = messagewithoutswear.replaceAll(word, SwearUtils.repeat("*", word.length()));
-                        if (!allowUsersToSeeWords) {
-                            event.setMessage(messagewithoutswear);
-                        }
-                        if (!SwearUtils.canSee(event.getPlayer())) {
-                            if (i == 0) {
-                                event.getPlayer().sendMessage(ChatColor.DARK_GREEN+"[BSwear] "+ChatColor.YELLOW + ChatColor.AQUA + ChatColor.BOLD +"We've detected a swear word MIGHT be in your message so we blocked that word!");
-                            }
-                            i++;
-                        }
-                    }
+	public static String getFromURL(final String urlString) throws MalformedURLException {
+		// Used for the Updater
+		BufferedInputStream in = null;
+		String output = "";
 
-                    List<String> l = log.getStringList("log");
-                    String a = event.getPlayer().getName() + " said " + word.toUpperCase() + " in message: " + event.getMessage();
-                    l.add(a);
-                    log.set("log", l);
-                    saveConf(log, logFile);
+		try {
+			in = new BufferedInputStream(new URL(urlString).openStream());
+			byte[] contents = new byte[1024];
 
-                    SwearUtils.checkAll(getConfig().getString("command"), event.getPlayer());
-                    event.setCancelled(true);
-                }
-            }//);
-            if (has) {
-                if (allowUsersToSeeWords) {
-                    event.setCancelled(true);
-                    if (!lastblockmsg.equalsIgnoreCase(event.getPlayer().getName() + "-" + message)) {
-                        for (Player p : Bukkit.getOnlinePlayers()) {
-                            if (SwearUtils.canSee(p)) {
-                                p.sendMessage(String.format(event.getFormat(), event.getPlayer().getDisplayName(), event.getMessage()));
-                            } else {
-                                p.sendMessage(String.format(event.getFormat(), event.getPlayer().getDisplayName(), messagewithoutswear));
-                            }
-                        }
-                    }
-                } else {
-                    event.setCancelled(false);
-                    event.setMessage(messagewithoutswear);
-                }
-                lastblockmsg = event.getPlayer().getName() + "-" + message;
-                Bukkit.getServer().getScheduler().runTaskLater(this, new Runnable() {
-                    @Override
-                    public void run() {
-                        lastblockmsg = "_null_";
-                    }}, 3);
-            }
-        }
-    }
+			int bytesRead = 0;
+			while ((bytesRead = in.read(contents)) != -1) {
+				output = new String(contents, 0, bytesRead);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return output;
+	}
 
-    public boolean ifHasWord(String message, String word) {
-        boolean a = false;
-        String[] messageAsArray = message.split(" ");
-        int messageLength = messageAsArray.length;
-        for (int i = 0; i < messageLength;) {
-            String partOfMessage = messageAsArray[i];
-            StringBuilder strBuilder = new StringBuilder();
-            char[] messageAsCharArray = partOfMessage.toLowerCase().toCharArray();
-            for (char character : messageAsCharArray) {
-                if(character>='0'&&character<='9'||character>='a'&&character<='z') strBuilder.append(character);
-            }
-            if (strBuilder.toString().equalsIgnoreCase(word.toLowerCase())) a = true;
-            i++;
-        }
-        return a;
-    }
+	public void checkForUpdate() {
+		String url = "https://raw.githubusercontent.com/BSwearTeam/BSwear/master/version";
+		try {
+			String versions = getFromURL(url);
+			String[] ver = versions.split("\n");
+			if (!ArrayUtils.contains(ver, version.toString())) {
+				getLogger().info("[=-=] BSwear Update [=-=]");
+				getLogger().info("An Update should be available.");
+				getLogger().info("Current Version: " + version.toString());
+				getLogger().info("New Version: " + ver[0]);
+			}else {
+				getLogger().info("BSwear is up-to-date");
 
-    /**
-     * Replaces all non-words and non-numbers.
-     */
-    public String replaceAllNotNormal(String str) {
-        return str.replaceAll("[^\\p{L}\\p{Nd}]","").replaceAll("[ * . - = + : ]","").replaceAll("[%&*()$#!-_@]","");
-    }
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    public void saveConf(FileConfiguration config, File file) {
-        try {
-            config.save(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-            getLogger().info("[ERROR] Cant save "+file.getName());
-        } 
-    }
+	}
 
-    private void resourceSave(File file,String fileName){
-        if(!file.exists()){file.getParentFile().mkdirs();saveResource(fileName,false);}
-    }
+	/* Controls config files */
+	public void saveSwearConfig() {
+		saveConf(swears, swearf);
+	}
+
+	public void saveSwearersConfig() {
+		saveConf(swearers, swearersf);
+	}
+
+	private String lastblockmsg = "_null_";
+
+	@EventHandler /* (priority = EventPriority.HIGHEST) */
+	public void onChatSwear(AsyncPlayerChatEvent event) {
+		if (true/* !event.getPlayer().hasPermission(BypassPerm) */) {
+			String message = replaceAllNotNormal(event.getMessage().toLowerCase().replaceAll("[%&*()$#!-_@]", ""));
+			boolean has = false;
+			boolean allowUsersToSeeWords = true; // TODO: add to configuration
+			String messagewithoutswear = message;
+			int i = 0;
+			/* swears.getStringList("warnList").stream().forEach((word) -> { */
+			for (String word : swears.getStringList("warnList")) {
+				if (ifHasWord(message, word)) {
+					has = true;
+					event.setCancelled(true); // BSwear handles sending the
+												// message so cancel the event.
+					if (getConfig().getBoolean("cancelMessage") == true) {
+						has = false; // cancel message.
+					} else {
+						messagewithoutswear = messagewithoutswear.replaceAll(word,
+								SwearUtils.repeat("*", word.length()));
+						if (!allowUsersToSeeWords) {
+							event.setMessage(messagewithoutswear);
+						}
+						if (!SwearUtils.canSee(event.getPlayer())) {
+							if (i == 0) {
+								event.getPlayer().sendMessage(ChatColor.DARK_GREEN + "[BSwear] " + ChatColor.YELLOW
+										+ ChatColor.AQUA + ChatColor.BOLD
+										+ "We've detected a swear word MIGHT be in your message so we blocked that word!");
+							}
+							i++;
+						}
+					}
+
+					List<String> l = log.getStringList("log");
+					String a = event.getPlayer().getName() + " said " + word.toUpperCase() + " in message: "
+							+ event.getMessage();
+					l.add(a);
+					log.set("log", l);
+					saveConf(log, logFile);
+
+					SwearUtils.checkAll(getConfig().getString("command"), event.getPlayer());
+					event.setCancelled(true);
+				}
+			} // );
+			if (has) {
+				if (allowUsersToSeeWords) {
+					event.setCancelled(true);
+					if (!lastblockmsg.equalsIgnoreCase(event.getPlayer().getName() + "-" + message)) {
+						for (Player p : Bukkit.getOnlinePlayers()) {
+							if (SwearUtils.canSee(p)) {
+								p.sendMessage(String.format(event.getFormat(), event.getPlayer().getDisplayName(),
+										event.getMessage()));
+							} else {
+								p.sendMessage(String.format(event.getFormat(), event.getPlayer().getDisplayName(),
+										messagewithoutswear));
+							}
+						}
+					}
+				} else {
+					event.setCancelled(false);
+					event.setMessage(messagewithoutswear);
+				}
+				lastblockmsg = event.getPlayer().getName() + "-" + message;
+				Bukkit.getServer().getScheduler().runTaskLater(this, new Runnable() {
+					@Override
+					public void run() {
+						lastblockmsg = "_null_";
+					}
+				}, 3);
+			}
+		}
+	}
+
+	public boolean ifHasWord(String message, String word) {
+		boolean a = false;
+		String[] messageAsArray = message.split(" ");
+		int messageLength = messageAsArray.length;
+		for (int i = 0; i < messageLength;) {
+			String partOfMessage = messageAsArray[i];
+			StringBuilder strBuilder = new StringBuilder();
+			char[] messageAsCharArray = partOfMessage.toLowerCase().toCharArray();
+			for (char character : messageAsCharArray) {
+				if (character >= '0' && character <= '9' || character >= 'a' && character <= 'z')
+					strBuilder.append(character);
+			}
+			if (strBuilder.toString().equalsIgnoreCase(word.toLowerCase()))
+				a = true;
+			i++;
+		}
+		return a;
+	}
+
+	/**
+	 * Replaces all non-words and non-numbers.
+	 */
+	public String replaceAllNotNormal(String str) {
+		return str.replaceAll("[^\\p{L}\\p{Nd}]", "").replaceAll("[ * . - = + : ]", "").replaceAll("[%&*()$#!-_@]", "");
+	}
+
+	public void saveConf(FileConfiguration config, File file) {
+		try {
+			config.save(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			getLogger().info("[ERROR] Cant save " + file.getName());
+		}
+	}
+
+	private void resourceSave(File file, String fileName) {
+		if (!file.exists()) {
+			file.getParentFile().mkdirs();
+			saveResource(fileName, false);
+		}
+	}
 }
