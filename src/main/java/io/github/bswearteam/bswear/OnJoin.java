a5ab548ce877a7f7b52af373ae520a8e86324384
ekclifford <clifforddj@verizon.net>
2018-01-13 10:15:26 -0500

    Updated Priorities

--- src/main/java/io/github/bswearteam/bswear/OnJoin.java
+++ src/main/java/io/github/bswearteam/bswear/OnJoin.java
@@ -3,13 +3,14 @@ package io.github.bswearteam.bswear;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class OnJoin implements Listener {
     private BSwear m;
     public OnJoin(BSwear main){this.m = main;}
 
-    @EventHandler
+    @EventHandler(priority=EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerJoinEvent e) {
         if (m.getConfig().getBoolean("showJoinMessage")) {
             int pWarn = SwearUtils.getPlrSwears(e.getPlayer());
