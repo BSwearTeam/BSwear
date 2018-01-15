62cd3ef664f1501a1ed2b2ff63d708c59f833387
IsaiahPatton <isaiahpatton1@gmail.com>
2018-01-15 11:43:34 -0500

    Fix CraftBukkit servers

--- src/main/java/io/github/bswearteam/bswear/AntiCaps.java
+++ src/main/java/io/github/bswearteam/bswear/AntiCaps.java
@@ -1,12 +1,11 @@
 package io.github.bswearteam.bswear;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import com.google.common.base.CharMatcher;
-
-import net.md_5.bungee.api.ChatColor;
 
 public class AntiCaps implements Listener {
     @EventHandler
